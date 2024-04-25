5a8152f90ed254191d9baa49173d7474fde1f8bc
Jhonatan Munoz <jhonatanm255@hotmail.com>
2024-04-25 12:58:48 -0400

    Ajuste del menu en resolucion max de 600px y ajuste de los elementos del main para dicha resolucion

--- main.js
+++ main.js
@@ -12,3 +12,20 @@ enlaces.forEach(enlace => {
         enlaceAnterior = enlace; 
     });
 });
+
+
+const btnMenu = document.getElementById('btn-menu-bar');
+const navRight = document.querySelector('.nav-right');
+const title = document.querySelector('.title');
+
+
+btnMenu.addEventListener('click', (e) => {
+    navRight.classList.toggle('show');
+    if (navRight.classList.contains('show') ) {
+        title.style.transform = 'translateY(170%)';
+    } else {
+        title.style.transform = 'translateY(0)'; // Volver el título a su posición original
+    }
+});
+
+

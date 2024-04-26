9c3685d1ef5080f4a4a43bc1cfcca455858ec52a
Jhonatan Munoz <jhonatanm255@hotmail.com>
2024-04-26 02:20:17 -0400

    insercion de los iconos de redes sociales y reajuste del elemento title, ademas de modificacion de la visual del menu

--- main.js
+++ main.js
@@ -22,9 +22,8 @@ const title = document.querySelector('.title');
 btnMenu.addEventListener('click', (e) => {
     navRight.classList.toggle('show');
     if (navRight.classList.contains('show') ) {
-        title.style.transform = 'translateY(170%)';
     } else {
-        title.style.transform = 'translateY(0)'; // Volver el título a su posición original
+        title.style.transform = 'translateY(0)';
     }
 });
 

74538597b5e0cb176e2f6e78d4153bb292df15c9
Jhonatan Munoz <jhonatanm255@hotmail.com>
2024-05-01 01:43:31 -0400

    creacion y aplicacion de ScrollReveal

---
+++ scrollreveal.js
@@ -0,0 +1,51 @@
+
+
+window.sr = ScrollReveal();
+
+sr.reveal('.head', {
+    origin: 'bottom',
+    distance: '-100px',
+    duration: 1000
+});
+
+sr.reveal('.banner-img', {
+    duration: 2000
+});
+
+sr.reveal('.title', {
+    origin: 'right',
+    distance: '-700px',
+    duration: 2000
+});
+
+sr.reveal('.card-1', {
+    duration: 1000
+});
+
+sr.reveal('.card-2', {
+    duration: 2000
+});
+
+sr.reveal('.card-3', {
+    duration: 3000
+});
+
+sr.reveal('.card-4', {
+    duration: 4000
+});
+
+sr.reveal('.seccion', {
+    duration: 2000
+});
+
+sr.reveal('.scroll-right', {
+    origin: 'right',
+    distance: '-100px',
+    duration: 2000
+});
+
+sr.reveal('.scroll-left', {
+    origin: 'left',
+    distance: '-100px',
+    duration: 2000
+});

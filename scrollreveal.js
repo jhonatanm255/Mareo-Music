6a40a0ca37e1e60831b312348f9814b3514468b4
Jhonatan Munoz <jhonatanm255@hotmail.com>
2024-05-06 03:21:46 -0400

    creacion de las secciones de conciertos en vivo

--- scrollreveal.js
+++ scrollreveal.js
@@ -1,61 +1,117 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-window.sr = ScrollReveal();
+    window.sr = ScrollReveal();
 
-sr.reveal('.head', {
-    origin: 'bottom',
-    distance: '-100px',
-    duration: 1000
-});
+    sr.reveal('.head', {
+        origin: 'bottom',
+        distance: '-100px',
+        duration: 1000
+    });
 
-sr.reveal('.banner-img', {
-    duration: 2000
-});
+    sr.reveal('.banner-img', {
+        duration: 2000
+    });
 
-sr.reveal('.title', {
-    origin: 'right',
-    distance: '-200px',
-    duration: 1000
-});
+    sr.reveal('.title', {
+        origin: 'right',
+        distance: '-200px',
+        duration: 1000
+    });
 
-sr.reveal('.title-cards', {
-    duration: 2000
-});
+    sr.reveal('.title-cards', {
+        duration: 2000
+    });
 
-sr.reveal('.cards', {
-    duration: 3000
-});
+    sr.reveal('.cards', {
+        duration: 3000
+    });
 
-sr.reveal('.card-2', {
-    duration: 2000
-});
+    sr.reveal('.card-2', {
+        duration: 2000
+    });
 
-sr.reveal('.card-3', {
-    duration: 3000
-});
+    sr.reveal('.card-3', {
+        duration: 3000
+    });
 
-sr.reveal('.card-4', {
-    duration: 4000
-});
+    sr.reveal('.card-4', {
+        duration: 4000
+    });
 
-sr.reveal('.footer-cards', {
-    duration: 2000
-});
+    sr.reveal('.footer-cards', {
+        duration: 2000
+    });
 
-sr.reveal('.seccion', {
-    duration: 2000
-});
+    // sr.reveal('.seccion', {
+    //     duration: 2000
+    // });
 
-sr.reveal('.scroll-right', {
-    origin: 'right',
-    distance: '-100px',
-    duration: 2000
-});
+    sr.reveal('.scroll-right', {
+        origin: 'right',
+        distance: '-100px',
+        duration: 2000
+    });
 
-sr.reveal('.scroll-left', {
-    origin: 'left',
-    distance: '-100px',
-    duration: 2000
-});
+    sr.reveal('.scroll-left', {
+        origin: 'left',
+        distance: '-100px',
+        duration: 2000
+    });
+
+    // EFECTO EN LA SECCION DE JAPAN TOUR
+
+    sr.reveal('.sec-1', {
+        origin: 'left',
+        distance: '-100px',
+        duration: 500
+    });
+
+    sr.reveal('.sec-2', {
+        origin: 'right',
+        distance: '-100px',
+        duration: 1000
+    });
+
+    sr.reveal('.sec-3', {
+        origin: 'left',
+        distance: '-100px',
+        duration: 1500
+    });
+
+    sr.reveal('.sec-4', {
+        origin: 'right',
+        distance: '-100px',
+        duration: 2000
+    });
+
+    sr.reveal('.sec-5', {
+        origin: 'left',
+        distance: '-100px',
+        duration: 2500
+    });
+
+    sr.reveal('.sec-6', {
+        origin: 'right',
+        distance: '-100px',
+        duration: 3000
+    });
+
+    sr.reveal('.sec-7', {
+        origin: 'left',
+        distance: '-100px',
+        duration: 3500
+    });
+
+    sr.reveal('.sec-8', {
+        origin: 'right',
+        distance: '-100px',
+        duration: 4000
+    });
+
+    // SECCION 2
+
+    sr.reveal('.seccion-concert', {
+        duration: 2000
+    });
 
 });
